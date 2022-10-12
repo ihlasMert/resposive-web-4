aa1b812492da4e20059b6e0b104dd01504efb26c
İHLASMERT <ihlas1mert@gmail.com>
2022-10-12 10:56:38 +0300

    added scroll bar

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ menu.onclick = () => {
 window.onscroll = () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
+  if (window.scrollY > 60){
+    document.querySelector("#scroll-top").classList.add("active");
+  }else{
+    document.querySelector("#scroll-top").classList.remove("active");
+  }
 };
